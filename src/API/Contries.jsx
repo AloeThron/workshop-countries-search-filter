85be96cf845496f755b961f6badf7770c70b5edb
AloeThron <lxe.tr.deat@gmail.com>
2024-01-11 23:55:22 +0700

    fix file

--- src/API/Contries.jsx
+++ src/API/Contries.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import Card from "../components/card";
 
 export default function ContriesAPI(props) {
   useEffect(() => {
